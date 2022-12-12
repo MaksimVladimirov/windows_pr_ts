97d414f7c68420e115443e0fa261babd19d7e56e
Maksim Vladimirov <vl4dimirov@gmail.com>
2022-12-12 21:17:21 +0300

    fix bugs

--- src/ts/modules/changeModalState.ts
+++ src/ts/modules/changeModalState.ts
@@ -1,6 +1,6 @@
 import { checkNumInputs } from "./checkNumInputs";
 
-export const changeModalState = (state: any) => {
+export const changeModalState = (state: { [key: string]: number | string }) => {
   const windowForms = document.querySelectorAll(".balcon_icons_img");
   const windowWidth = document.querySelectorAll("#width");
   const windowHeight = document.querySelectorAll("#height");
@@ -10,7 +10,7 @@ export const changeModalState = (state: any) => {
   checkNumInputs("#width");
   checkNumInputs("#height");
 
-  const bindActionToElems = (event: any, element: any, property: any) => {
+  const bindActionToElems = (event: string, element: any, property: string) => {
     element.forEach((item: any, index: number) => {
       item.addEventListener(event, () => {
         switch (item.nodeName) {
@@ -19,24 +19,13 @@ export const changeModalState = (state: any) => {
             break;
           case "INPUT":
             if (item.getAttribute("type") === "checkbox") {
-              index === 0
-                ? (state[property] = "Холодное")
-                : (state[property] = "Теплое");
-              element.forEach((box: any, ind: number) => {
-                box.checked = false;
-                if (index == ind) {
-                  box.checked = true;
-                }
-              });
-            } else {
-              state[property] = item.value;
+              index === 0 ? (state[property] = "Холодное") : (state[property] = "Теплое");
             }
-            break;
           case "SELECT":
             state[property] = item.value;
             break;
         }
-        console.log(state)
+        console.log(state);
       });
     });
   };
