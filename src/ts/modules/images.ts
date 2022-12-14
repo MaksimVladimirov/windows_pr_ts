c3d14301f8486f8e1a420e9e1ffb2768e6815dfa
Maksim Vladimirov <vl4dimirov@gmail.com>
2022-12-14 10:44:00 +0300

    fix bugs

--- src/ts/modules/images.ts
+++ src/ts/modules/images.ts
@@ -1,30 +1,30 @@
 export const images = () => {
-    const imgPopup = document.createElement('div'),
-          workSection = document.querySelector('.works'),
-          bigImage = document.createElement('img');
+  const imgPopup = document.createElement("div");
+  const workSection = document.querySelector(".works");
+  const bigImage = document.createElement("img");
 
-    imgPopup.classList.add('popup');
-    workSection.appendChild(imgPopup);
+  imgPopup.classList.add("popup");
+  workSection.appendChild(imgPopup);
 
-    imgPopup.style.justifyContent = 'center';
-    imgPopup.style.alignItems = 'center';
-    imgPopup.style.display = 'none';
+  imgPopup.style.justifyContent = "center";
+  imgPopup.style.alignItems = "center";
+  imgPopup.style.display = "none";
 
-    imgPopup.appendChild(bigImage);
+  imgPopup.appendChild(bigImage);
 
-    workSection.addEventListener('click', (e:any) => {
-        e.preventDefault();
+  workSection.addEventListener("click", (e: any) => {
+    e.preventDefault();
 
-        let target = e.target;
+    const target = e.target;
 
-        if (target && target.classList.contains('preview')) {
-            imgPopup.style.display = 'flex';
-            const path = target.parentNode.getAttribute('href');
-            bigImage.setAttribute('src', path);
-        }
+    if (target && target.classList.contains("preview")) {
+      imgPopup.style.display = "flex";
+      const path = target.parentNode.getAttribute("href");
+      bigImage.setAttribute("src", path);
+    }
 
-        if (target && target.matches('div.popup')) {
-            imgPopup.style.display = 'none';
-        }
-    });
+    if (target && target.matches("div.popup")) {
+      imgPopup.style.display = "none";
+    }
+  });
 };
